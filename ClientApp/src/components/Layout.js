4de351ec55d37428b419ec8466e54fffdddc35b5
Dustin Dixon <bobdeus@gmail.com>
2021-04-28 19:25:55 -0600

    update content for project title and change icon

--- ClientApp/src/components/Layout.js
+++ ClientApp/src/components/Layout.js
@@ -2,17 +2,15 @@ import React, { Component } from 'react';
 import { Container } from 'reactstrap';
 import { NavMenu } from './NavMenu';
 
-export class Layout extends Component {
-  static displayName = Layout.name;
+export const Layout = (props) => {
+  const displayName = Layout.name;
 
-  render () {
-    return (
-      <div>
-        <NavMenu />
-        <Container>
-          {this.props.children}
-        </Container>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <NavMenu />
+      <Container>
+        {props.children}
+      </Container>
+    </div>
+  );
+};
